9f834510c9eba539fb737f50c63c4b9762446bf4
Kenny Zhao <haise.zhao@gmail.com>
2022-12-08 21:18:51 -0800

    Add feature for user input to generate variable size grid
    Remove fixed dimensions for box from CSS. The size of the box is now set in Javascript based on dimensions of sketchbox.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,30 @@
-//Function to generate a 16x16 grid
+//Function to generate n size grid
 function createGrid() {
     
     const sketchBox = document.querySelector(".sketchbox");
-    sketchBox.innerHTML = "";
-    
-    for (let i = 0; i < 16; i++) {
+    sketchBox.innerHTML = ""; //Clears sketchbox
+
+    const width = sketchBox.offsetWidth;
+
+    const n = Number(prompt("Enter a number 1-100 to generate a grid", ""));
+
+    for (let i = 0; i < n; i++) {
         //Create a row container at the start of every row and set as flex container
         const row = document.createElement("div");
         row.classList.toggle("row");
 
         //Fill the row container with boxes
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < n; j++) {
             const box = document.createElement("div");
             box.classList.toggle("box");
 
-            //When mouseover, change color of the box;
-            box.addEventListener("mouseover", () => box.classList.add("hover"))
+            //Set width and height of the box based on size of sketch box
+            box.style.width = `${width/n}px`
+            box.style.height = `${width/n}px`
 
+            //When mouseover, change color of the box;
+            box.addEventListener("mouseover", () => box.classList.add("hover"));
+            
             row.appendChild(box);
         }
         //Add the row full of boxes into the sketchbox before starting on the next row
