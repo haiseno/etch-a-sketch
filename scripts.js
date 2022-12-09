a12fd5692aa631d4dbf1b1bceb3c5c69311a71be
Kenny Zhao <haise.zhao@gmail.com>
2022-12-08 18:39:23 -0800

    Add event listener for changing box color on hover

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 //Function to generate a 16x16 grid
 function createGrid() {
-
+    
     const sketchBox = document.querySelector(".sketchbox");
-
+    
     for (let i = 0; i < 16; i++) {
-
         //Create a row container at the start of every row and set as flex container
         const row = document.createElement("div");
         row.classList.toggle("row");
@@ -13,9 +12,12 @@ function createGrid() {
         for (let j = 0; j < 16; j++) {
             const box = document.createElement("div");
             box.classList.toggle("box");
+
+            //When mouseover, change color of the box;
+            box.addEventListener("mouseover", () => box.classList.add("hover"))
+
             row.appendChild(box);
         }
-        
         //Add the row full of boxes into the sketchbox before starting on the next row
         sketchBox.append(row);
     }
